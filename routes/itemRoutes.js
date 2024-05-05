c3ec246e248c827b845919a8bc5ce9501f9cdf28
Arshit790 <arshitdixit5@gmail.com>
2024-05-05 20:05:32 +0530

    "add-item using post"

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express')
-const { getItemController } = require('../controllers/itemController')
+const { getItemController,addItemController } = require('../controllers/itemController')
 
 const router = express.Router()
 
 //routes
+
 //Method - Get
 router.get('/get-item',getItemController)
+
+//Method - Post
+router.get('/add-item',addItemController)
+
 
 module.exports = router
