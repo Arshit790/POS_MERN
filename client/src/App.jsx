d5059fdd192592469c30f92101dde032b07733c7
Arshit790 <arshitdixit5@gmail.com>
2024-05-06 12:08:23 +0530

    "workking on ItemList compo"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
-import "antd/dist/antd.min.css"
+import "antd/dist/antd.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import ItemPage from "./pages/ItemPage";
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />}></Route>
-          <Route path="/items" element={<ItemPage />}></Route>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/items" element={<ItemPage />} />
         </Routes>
       </BrowserRouter>
     </>
