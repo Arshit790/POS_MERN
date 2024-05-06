55aa4f1371a232fa97c0db3f7ef2ce39be9f671d
Arshit790 <arshitdixit5@gmail.com>
2024-05-06 16:29:05 +0530

    "add code"

--- client/src/redux/RootReducer.jsx
+++ client/src/redux/RootReducer.jsx
@@ -1,1 +1,9 @@
-consr
+const initialState = {
+  loading : false,
+  cartItems :[]
+}
+export const RootReducer = (state = initialState,action) => {
+  switch(action.type){
+    default : return state
+  }
+}
