a35831ab55c797d27ccbc16beeca5abb90b767c9
Arshit790 <arshitdixit5@gmail.com>
2024-05-06 17:08:53 +0530

    "add buttons"

--- client/src/components/ItemList.jsx
+++ client/src/components/ItemList.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Card } from "antd";
+import { Button,Card } from "antd";
 
 const ItemList = ({ item }) => {
  
-  // //update cart handler
-  // const handleAddTOCart = () => {
-  //   dispatch({
-  //     type: "ADD_TO_CART",
-  //     payload: item,
-  //   });
-  // };
+  //update cart handler
+  const handleAddTOCart = () => {
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: item,
+    });
+  };
   const { Meta } = Card;
   return (
     <div>
@@ -18,9 +18,9 @@ const ItemList = ({ item }) => {
         cover={<img alt={item.name} src={item.image} style={{ height: 200 }} />}
       >
         <Meta title={item.name} />
-        {/* <div className="item-button">
+        <div className="item-button">
           <Button onClick={() => handleAddTOCart()}>Add to cart</Button>
-        </div> */}
+        </div>
       </Card>
     </div>
   );
