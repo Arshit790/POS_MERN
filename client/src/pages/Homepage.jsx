9adcafac64f7543233f984281f90f677fe7f43b0
Arshit790 <arshitdixit5@gmail.com>
2024-05-06 13:11:18 +0530

    "some updates in code"

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DefaultLayout from "../components/DefaultLayout";
+import axios from "axios";
+import { Row, Col } from "antd";
 
 const Homepage = () => {
+  const [itemsData, setItemsData] = useState([]);
+
+  //useffect
+  useEffect(() => {
+    const getAllItems = async () => {
+      try {
+        const { data } = await axios.get("/api/items/get-item");
+        setItemsData(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllItems();
+  }, []);
   return (
     <DefaultLayout>
-      <h1>HomePage</h1>
+      <Row>
+        {itemsData.map((item) => (
+          <Col xs={24} lg={6} md={12} sm={6}>
+            <ItemList item={item} />
+          </Col>
+        ))}
+      </Row>
     </DefaultLayout>
   );
 };
