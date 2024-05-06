7598d34d5e777fa13a248b082a2dd2bfb2e9c658
Arshit790 <arshitdixit5@gmail.com>
2024-05-06 12:59:43 +0530

    "adding css to index.css"

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import DefaultLayout from "../components/DefaultLayout";
 
 const Homepage = () => {
   return (
-    <div>
+    <DefaultLayout>
       <h1>HomePage</h1>
-    </div>
+    </DefaultLayout>
   );
 };
 
