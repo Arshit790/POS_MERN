9ab224181c037b6f7fd67f4c58f2b0da46bbe290
Arshit790 <arshitdixit5@gmail.com>
2024-05-07 20:57:01 +0530

    "working on login/register page"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import ItemPage from "./pages/ItemPage";
 import CartPage from "./pages/CartPage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 function App() {
   return (
     <>
@@ -11,6 +13,8 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/items" element={<ItemPage />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
