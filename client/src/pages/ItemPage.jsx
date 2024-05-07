1bfb578a9a55284a8f1be66fcc53856109777470
Arshit790 <arshitdixit5@gmail.com>
2024-05-07 11:32:15 +0530

    "wroking on itemList functionality"

--- client/src/pages/ItemPage.jsx
+++ client/src/pages/ItemPage.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import DefaultLayout from "../components/DefaultLayout";
 import { useDispatch } from "react-redux";
+import axios from "axios";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { Modal, Button, Table, Form, Input, Select, message } from "antd";
+
 const ItemPage = () => {
   const [itemsData, setItemsData] = useState([]);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const [popupModal, setPopupModal] = useState(false);
 
   //useffect
   useEffect(() => {
@@ -24,9 +29,84 @@ const ItemPage = () => {
     };
     getAllItems();
   }, [dispatch]);
+
+  // table data
+  const columns = [
+    { title: "Name", dataIndex: "name" },
+    {
+      title: "Image",
+      dataIndex: "image",
+      render: (image, record) => (
+        <img src={image} alt={record.name} height="60" width="60" />
+      ),
+    },
+    { title: "Price", dataIndex: "price" },
+
+    {
+      title: "Actions",
+      dataIndex: "_id",
+      render: (id, record) => (
+        <div>
+          <DeleteOutlined style={{ cursor: "pointer" }} />
+          <EditOutlined style={{ cursor: "pointer" }} />
+        </div>
+      ),
+    },
+  ];
+
+  //handle submit
+  const handleSubmit = () => {
+    
+  }
+
   return (
     <DefaultLayout>
-      <h1>ItemsPage</h1>
+      <div className="d-flex justify-content-between">
+        <h1>Item List</h1>
+        <Button type="primary" onClick={() => setPopupModal(true)}>
+          Add Item
+        </Button>
+      </div>
+      <Table columns={columns} dataSource={itemsData} bordered />
+
+      <Modal
+        title={"Add New Item"}
+        visible={popupModal}
+        onCancel={() => {
+          // setEditItem(null);
+          setPopupModal(false);
+        }}
+        footer={false}
+      >
+        <Form
+          layout="vertical"
+          // initialValues={editItem}
+          onFinish={handleSubmit}
+        >
+          <Form.Item name="name" label="Name">
+            <Input />
+          </Form.Item>
+          <Form.Item name="price" label="Price">
+            <Input />
+          </Form.Item>
+          <Form.Item name="image" label="Image URL">
+            <Input />
+          </Form.Item>
+          <Form.Item name="category" label="Category">
+            <Select>
+              <Select.Option value="drinks">Drinks</Select.Option>
+              <Select.Option value="rice">Rice</Select.Option>
+              <Select.Option value="noodles">Noodels</Select.Option>
+            </Select>
+          </Form.Item>
+
+          <div className="d-flex justify-content-end">
+            <Button type="primary" htmlType="submit">
+              SAVE
+            </Button>
+          </div>
+        </Form>
+      </Modal>
     </DefaultLayout>
   );
 };
