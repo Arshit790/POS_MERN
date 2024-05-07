a14a70d8073cc48886411f2ed26db8c6d27e716f
Arshit790 <arshitdixit5@gmail.com>
2024-05-07 22:03:17 +0530

    "bill adding"

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DefaultLayout from "../components/DefaultLayout";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -6,9 +6,12 @@ import {
   PlusCircleOutlined,
   MinusCircleOutlined,
 } from "@ant-design/icons";
-import { Table } from "antd";
+import { Button, Table, Modal, message, Form, Input, Select } from "antd";
+import useSelection from "antd/es/table/hooks/useSelection";
 
 const CartPage = () => {
+  const [subTotal, setSubTotal] = useState(0);
+  const [billPopup, setBillPopup] = useState(false);
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.RootReducer);
   //handle increment
@@ -63,17 +66,100 @@ const CartPage = () => {
       render: (id, record) => (
         <DeleteOutlined
           style={{ cursor: "pointer" }}
-          onClick={() => dispatch({
-            type : 'DELETE_FROM_CART',
-            payload : record
-          })}
+          onClick={() =>
+            dispatch({
+              type: "DELETE_FROM_CART",
+              payload: record,
+            })
+          }
         />
       ),
     },
   ];
+
+  useEffect(() => {
+    let temp = 0;
+    cartItems.forEach((item) => (temp = temp + item.price * item.quantity));
+    setSubTotal(temp);
+  }, [cartItems]);
+
+  //handle submit
+  const handleSubmit = async (value) => {
+    try {
+      const newObject = {
+        ...value,
+        cartItems,
+        subTotal,
+        tax: Number(((subTotal / 100) * 10).toFixed(2)),
+        totalAmount: Number(
+          Number(subTotal) + Number(((subTotal / 100) * 10).toFixed(2))
+        ),
+        userId: JSON.parse(localStorage.getItem("auth"))._id,
+      };
+      // console.log(newObject);
+      await axios.post("/api/bills/add-bills", newObject);
+      message.success("Bill Generated");
+      navigate("/bills");
+    } catch (error) {
+      message.error("Something went wrong");
+      console.log(error);
+    }
+  };
+
   return (
     <DefaultLayout>
       <Table columns={columns} dataSource={cartItems} bordered />
+      <div className="d-flex flex-column align-items-end">
+        <hr />
+        <h3>
+          SUB TOTAL : $ <b>{subTotal}</b> /-
+        </h3>
+        <Button type="primary" onClick={() => setBillPopup(true)}>
+          Create Invoice
+        </Button>
+      </div>
+      <Modal
+        title="Create Invoice"
+        visible={billPopup}
+        footer={false}
+        onCancel={() => setBillPopup(false)}
+      >
+        <Form layout="vertical" onFinish={handleSubmit}>
+          <Form.Item name="customerName" label="Customer Name">
+            <Input />
+          </Form.Item>
+          <Form.Item name="customerNumber" label="Contact Number">
+            <Input />
+          </Form.Item>
+
+          <Form.Item name="paymentMode" label="Payment Method">
+            <Select>
+              <Select.Option value="cash">Cash</Select.Option>
+              <Select.Option value="card">Card</Select.Option>
+            </Select>
+          </Form.Item>
+          <div className="bill-it">
+            <h5>
+              Sub Total : <b>{subTotal}</b>
+            </h5>
+            <h4>
+              TAX
+              <b> {((subTotal / 100) * 10).toFixed(2)}</b>
+            </h4>
+            <h3>
+              GRAND TOTAL -{" "}
+              <b>
+                {Number(subTotal) + Number(((subTotal / 100) * 10).toFixed(2))}
+              </b>
+            </h3>
+          </div>
+          <div className="d-flex justify-content-end">
+            <Button type="primary" htmlType="submit">
+              Generate Bill
+            </Button>
+          </div>
+        </Form>
+      </Modal>
     </DefaultLayout>
   );
 };
